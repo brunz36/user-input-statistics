9f3ec084003e1df528b98d0a079ea6facf53586e
Bruno Ribeiro <brunz36@gmail.com>
2017-03-20 21:55:01 -0400

    Made new progrom using loop do and for each loop

--- app.rb
+++ app.rb
@@ -1,29 +1,25 @@
-puts "This program will ask for 5 numbers and output the sum and average."
+puts "Please enter any numbers you like. After each one press enter, when leave field blank and press enter."
 
-print "Please enter the first number: "
-num1 = gets.chomp
+numbers = []
 
-print "Please enter the second number: "
-num2 = gets.chomp
+loop do
+  print "\nPlease type any number you desire: "
+  input = gets
+  if input == "\n"
+    puts "\nThank you for your input."
+    puts "\n"
+    break
+  else
+    input = input.to_f
+    numbers.push(input)
+  end
+end
 
-print "Please enter the third number: "
-num3 = gets.chomp
+sum = 0
 
-print "Please enter the fourth number: "
-num4 = gets.chomp
+numbers.each { |x| sum += x}
+avg = sum / numbers.length
 
-print "Please enter the fifth number: "
-num5 = gets.chomp
-
-num1 = num1.to_f
-num2 = num2.to_f
-num3 = num3.to_f
-num4 = num4.to_f
-num5 = num5.to_f
-
-sum = num1 + num2 + num3 + num4 + num5
-
-avg = sum / 5
-
-puts "The sum of your input is: #{sum}"
-puts "The average of your input is: #{avg}"
+puts "The sum of your input is #{sum}."
+puts "The average of #{numbers} is #{avg}."
+puts "\n"
