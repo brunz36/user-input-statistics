81e4b205501bb34521e31373ffd90528d45ef32f
Bruno Ribeiro <brunz36@gmail.com>
2017-03-21 00:38:03 -0400

    Semi completed Epic Mode, no user input validation

--- app.rb
+++ app.rb
@@ -1,25 +1,65 @@
-puts "Please enter any numbers you like. After each one press enter, when leave field blank and press enter."
+puts "Please choose what type of array(list) you would like to work with.\n'S' for strings and 'N' for numbers."
+puts %{
+  S: String -- Words
+  N: Number -- 36, -42 or 3.14
+}
+print ">> "
+selected = gets.chomp.downcase
 
-numbers = []
+def string
+  puts "Please enter type a word and press enter. When you don't want to add anymore just leave it blank and press enter."
 
-loop do
-  print "\nPlease type any number you desire: "
-  input = gets
-  if input == "\n"
-    puts "\nThank you for your input."
-    puts "\n"
-    break
-  else
-    input = input.to_f
-    numbers.push(input)
+  string = ""
+
+  loop do
+    print "\nPlease type any word you desire: "
+    input = gets.chomp
+    if input == ""
+      puts "\nThank you for your input."
+      puts "\n"
+      break
+    # elsif input.is_a?(Numeric) == true
+    #   puts "Please type a valid string(word)."
+    #   break
+    else
+      string += input
+    end
   end
+  puts string
 end
 
-sum = 0
+def numbers
+  puts "Please enter any numbers you like. After each one press enter, when leave field blank and press enter."
 
-numbers.each { |x| sum += x}
-avg = sum / numbers.length
+  numbers = []
 
-puts "The sum of your input is #{sum}."
-puts "The average of #{numbers} is #{avg}."
-puts "\n"
+  loop do
+    print "\nPlease type any number you desire: "
+    input = gets
+    if input == "\n"
+      puts "\nThank you for your input."
+      puts "\n"
+      break
+    else
+      input = input.to_f
+      numbers.push(input)
+    end
+  end
+
+  sum = 0
+
+  numbers.each { |x| sum += x}
+  avg = sum / numbers.length
+
+  puts "The sum of your input is #{sum}."
+  puts "The average of #{numbers} is #{avg}."
+  puts "\n"
+end
+
+if selected == "s"
+  string
+elsif selected == "n"
+  numbers
+else
+  puts "Please only select S or N"
+end
